dc6936f499429743d186b076b1abee179f272cdc
shanole <73411129+shanole@users.noreply.github.com>
2021-08-02 20:42:48 -0700

    started NewTicketForm class
    Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,11 +1,24 @@
 import React from "react";
 
-function NewTicketForm(props){
-  return(
-    <React.Fragment>
-      <h3>this is a form.</h3>
-    </React.Fragment>
-  );
+class NewTicketForm extends  React.Component {
+  constructor (props) {
+    super(props);
+    this.state ={
+      debuggingLesson : false,
+      pairHelp : false,
+      documenting : false, 
+    };
+  }
+
+  // create separate components for 3 pages
+  render() {
+    return(
+      <React.Fragment>
+        <h3>this is a form.</h3>
+      </React.Fragment>
+    );
+  } 
+
 }
 
 export default NewTicketForm;
